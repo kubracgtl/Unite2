81ec27d1f3e702f6bdeec4c9d0591b76b7bddf00
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-10-11 11:49:53 +0300

    no message

--- app/src/main/java/com/example/unite2/MainActivity.java
+++ app/src/main/java/com/example/unite2/MainActivity.java
@@ -16,27 +16,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void uyg2Goster(View view) {
-        Intent i =new Intent(MainActivity.this, Uyg2Activity.class);
+        Intent i = new Intent(MainActivity.this, Uyg2Activity.class);
         startActivity(i);
     }
+
     public void uyg4Goster(View view) {
-        Intent i =new Intent(MainActivity.this, Uyg4Activity.class);
+        Intent i = new Intent(MainActivity.this, Uyg4Activity.class);
         startActivity(i);
     }
 
     public void SS64GOSTER(View view) {
-        Intent i =new Intent(MainActivity.this, SS64Activity.class);
+        Intent i = new Intent(MainActivity.this, SS64Activity.class);
         startActivity(i);
     }
 
     public void tamamgoster(View view) {
 
-        Intent i =new Intent(MainActivity.this, ss65Activity.class);
+        Intent i = new Intent(MainActivity.this, ss65Activity.class);
         startActivity(i);
     }
 
-    public void uyg5Goster(View view) {
-        Intent i =new Intent(MainActivity.this, uyg_5Activity.class);
+    public void uyg8Goster(View view) {
+        Intent i = new Intent(MainActivity.this, UYG8Activity.class);
         startActivity(i);
     }
+
+    public void uyg9Goster(View view) {
+        Intent i = new Intent(MainActivity.this, UYG9Activity.class);
+        startActivity(i);
+    }
+        public void uyg10goster(View view) {
+            Intent i = new Intent(MainActivity.this, uyg10Activity.class);
+            startActivity(i);
+
+    }
 }
+
